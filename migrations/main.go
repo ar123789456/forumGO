49ad1aa725826760104f4af8b36de8562521f110
Arman123 <Arman123>
2022-01-04 01:10:01 +0600

    add migrations

--- migrations/main.go
+++ migrations/main.go
@@ -1,1 +1,27 @@
 package migrations
+
+import (
+	"database/sql"
+	"forum/config"
+	"log"
+)
+
+//Run migrates
+func Run() {
+	migrate(config.DB, Notes)
+}
+
+func migrate(dbDriver *sql.DB, query string) {
+	statement, err := dbDriver.Prepare(query)
+	if err == nil {
+		_, creationError := statement.Exec()
+		if creationError == nil {
+			log.Println("Table created successfully")
+		} else {
+			log.Println(creationError.Error())
+		}
+	} else {
+		log.Println(err.Error())
+	}
+
+}
