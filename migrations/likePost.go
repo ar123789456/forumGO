7d5a36cf68ec3a49b4e3aad352ad2e112f09519c
Arman123 <Arman123>
2022-01-05 16:27:38 +0600

    migration> del like Id

--- migrations/likePost.go
+++ migrations/likePost.go
@@ -2,7 +2,6 @@ package migrations
 
 const LikePost string = `
 CREATE TABLE "likes_posts" (
-	"id"	INTEGER NOT NULL UNIQUE,
 	"id_post"	INTEGER NOT NULL,
 	"id_user"	INTEGER NOT NULL,
 	FOREIGN KEY("id_user") REFERENCES "user"("ID"),
