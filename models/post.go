bfa1fb52ff67b2a78208fb0d5b25edf28c4500d4
Arman123 <Arman123>
2022-01-18 15:51:05 +0600

    finished all models/post method

--- models/post.go
+++ models/post.go
@@ -3,6 +3,7 @@ package models
 import (
 	"forum/config"
 	"log"
+	"net/http"
 	"time"
 )
 
@@ -23,8 +24,17 @@ type PostParam struct {
 	// tags        []int
 }
 
+func (param *PostParam) Parse(r *http.Request) error {
+	err := r.ParseForm()
+	param.Title = r.FormValue("Title")
+	param.Content = r.FormValue("Content")
+	param.User_id = 1
+	log.Println("Models/PostParams:", err)
+	return err
+}
+
 func (post *Post) CREATE(userInput PostParam) (*Post, error) {
-	statement, _ := config.DB.Prepare("INSERT INTO posts(title, content, create_at, update_at, id_user) VALUES();")
+	statement, _ := config.DB.Prepare("INSERT INTO posts(title, content, create_at, update_at, id_user) VALUES(?, ?, ?, ?, ?);")
 	time := time.Now().String()
 	result, err := statement.Exec(userInput.Title, userInput.Content, time, time, userInput.User_id)
 	if err == nil {
@@ -39,4 +49,51 @@ func (post *Post) CREATE(userInput PostParam) (*Post, error) {
 	}
 	log.Println("Unable to create post:", err)
 	return post, err
+}
+
+func (post *Post) UPDATE(userInput PostParam, id int) (*Post, error) {
+	// ToDo add category_id
+	statement, _ := config.DB.Prepare("UPDATE posts SET title = ?, content = ?, update_at = ? WHERE id = ?;")
+	time := time.Now().String()
+	_, err := statement.Exec(userInput.Title, userInput.Content, time, id)
+	if err == nil {
+		post.GET(id)
+		return post, err
+	}
+	log.Println("Unable to create post:", err)
+	return post, err
+}
+
+func (*Post) DELETE(id int) error {
+	statement, _ := config.DB.Prepare("DELETE FROM posts WHERE id=?")
+	_, err := statement.Exec(id)
+	return err
+}
+
+func (post *Post) GET(id int) (*Post, error) {
+	err := config.DB.QueryRow(
+		"SELECT id, title, content, create_at, update_at, id_user FROM posts WHERE id=?", id).Scan(
+		&post.Id, &post.Title, &post.Content, &post.Creat_at, &post.Update_to, &post.User_id)
+	return post, err
+}
+
+func (*Post) GETALL() ([]Post, error) {
+	rows, err := config.DB.Query("SELECT * FROM posts")
+	var posts []Post
+	if err == nil {
+		for rows.Next() {
+			var currentPost Post
+			rows.Scan(
+				&currentPost.Id,
+				&currentPost.Title,
+				&currentPost.Content,
+				&currentPost.Creat_at,
+				&currentPost.Update_to,
+				&currentPost.User_id,
+			)
+			posts = append(posts, currentPost)
+		}
+		return posts, err
+	}
+	return posts, err
 }
